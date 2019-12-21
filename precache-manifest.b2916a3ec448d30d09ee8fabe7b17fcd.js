a938a0ae9765ea708bb359ed06b881cd6c35aec3
Dmitry <e965@protonmail.com>
2019-12-22 01:20:58 +0500

    Updates

---
+++ precache-manifest.b2916a3ec448d30d09ee8fabe7b17fcd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd449edf32211c35c129559478e51846",
+    "url": "/v1336-test-task/index.html"
+  },
+  {
+    "revision": "765e18320c39d697c726",
+    "url": "/v1336-test-task/static/css/2.02974078.chunk.css"
+  },
+  {
+    "revision": "7a6205271815749fa834",
+    "url": "/v1336-test-task/static/css/main.1747d620.chunk.css"
+  },
+  {
+    "revision": "765e18320c39d697c726",
+    "url": "/v1336-test-task/static/js/2.155d0994.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/v1336-test-task/static/js/2.155d0994.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a6205271815749fa834",
+    "url": "/v1336-test-task/static/js/main.dcee0918.chunk.js"
+  },
+  {
+    "revision": "c5a0482cfcbdd6b88e92",
+    "url": "/v1336-test-task/static/js/runtime-main.88e316f6.js"
+  },
+  {
+    "revision": "35e77a38ca9d85c4e8974261be764879",
+    "url": "/v1336-test-task/static/media/forkawesome-webfont.35e77a38.eot"
+  },
+  {
+    "revision": "3a9e014c2469ffa65a0ea64a24e48b44",
+    "url": "/v1336-test-task/static/media/forkawesome-webfont.3a9e014c.woff2"
+  },
+  {
+    "revision": "44bbdbbfb5a10ba2d1ceba3c1e0fc1f5",
+    "url": "/v1336-test-task/static/media/forkawesome-webfont.44bbdbbf.woff"
+  },
+  {
+    "revision": "78dcc9c4999659b8026a0c5213941d43",
+    "url": "/v1336-test-task/static/media/forkawesome-webfont.78dcc9c4.svg"
+  },
+  {
+    "revision": "fc46f3dae03b2b2e1ceed60971e90c9a",
+    "url": "/v1336-test-task/static/media/forkawesome-webfont.fc46f3da.ttf"
+  }
+]);
